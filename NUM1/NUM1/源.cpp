e5b7372d316b33ea4060bba40822fd677792e6c0
YAX <1967575938@qq.com>
2023-02-12 17:34:00 +0800

    v1.1

--- NUM1/NUM1/源.cpp
+++ NUM1/NUM1/源.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+using namespace std;
+struct Student {
+	string name;//姓名
+	int age;//年龄
+	int score;//分数
+
+};
+
 
 int main() {
-	std::cout << "hello" << std::endl;
+	
+	Student s1;
+	s1.name = "Tom";
+	s1.age = 10;
+	s1.score = 100;
 
+	cout << s1.name << s1.age << s1.score;
 	return 0;
 
 }
