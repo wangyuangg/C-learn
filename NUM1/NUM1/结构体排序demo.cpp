782b481a75439a07848c71bf14713c5e9a53d3b7
YAX <1967575938@qq.com>
2023-02-12 21:25:03 +0800

    1.6

--- NUM1/NUM1/结构体排序demo.cpp
+++ NUM1/NUM1/结构体排序demo.cpp
@@ -2,16 +2,40 @@
 using namespace  std;
 #include<string>
 
-struct hero {
+struct hero 
+{
 	string name;//姓名
 	int age;//年龄
 	string sex;
 
 };
+void bubbleSort(struct hero heroArray[], int len) 
+{
+	for (int i = 0; i < len - 1; i++) 
+	{
+		for (int j = 0; j < len - i - 1; j++)
+		{
+			if (heroArray[j].age > heroArray[j + 1].age)
+			{
+				struct hero temp = heroArray[j];
+				heroArray[j] = heroArray[j + 1];
+				heroArray[j + 1] = temp;
+			}
+		}
 
+
+	}
+}
+void printArray(struct hero heroArray[], int len)
+{
+	for (int i = 0; i < len; i++) {
+		cout << heroArray[i].name << "年龄" << heroArray[i].age << endl;
+	}
+}
 
 int main() {
-	hero heroArray[5] = {
+	hero heroArray[5] = 
+	{
 		{"刘备",23,"男"},
 		{"关羽",22,"男"},
 		{"张飞",20,"男"},
@@ -19,9 +43,11 @@ int main() {
 		{"镖产",19,"女"},
 	};
 	int len = sizeof(heroArray) / sizeof(heroArray[0]);
-	for (int i = 0; i < len; i++) {
-		cout << heroArray[i].name << endl;
-	}
+	//for (int i = 0; i < len; i++) {
+	//	cout << heroArray[i].name << endl;
+	//}
+	bubbleSort(heroArray, len);
+	printArray(heroArray, len);
 	system("pause");
 	return 0;
 }
