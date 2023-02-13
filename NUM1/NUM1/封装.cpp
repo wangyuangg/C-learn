e8ebfcf92985931c977aa25d8256ebe1f72d4974
YAX <1967575938@qq.com>
2023-02-13 21:05:27 +0800

    v1.9

---
+++ NUM1/NUM1/封装.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+const double PI = 3.14;
+class Circle
+{
+
+public:
+
+	int r;//°ë¾¶
+	double get()
+	{
+		return 2 * PI * r;
+	}
+	
+};
+
+int main()
+{
+	Circle Cone;
+	Cone.r = 10;
+	int result = Cone.get();
+	cout << result;
+	system("pause");
+	return 0;
+
+
+}
